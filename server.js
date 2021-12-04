31d8de87cafd495b69e2ffe08c6318fd3ab58a9a
Harsh Dobariya <91943309+Harsh-Dobariya@users.noreply.github.com>
2021-12-04 19:17:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
-// server.js
-// where your node app starts
-
 // init project
 require('dotenv').config();
 const express = require('express');
 const app = express();
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
 app.get("/api/whoami", (req, res) => {
   const result = {
     ipaddress: req.headers['x-forwarded-for'],
@@ -25,8 +18,5 @@ app.get("/api/whoami", (req, res) => {
   res.json(result);
 });
 
-
-
-// listen for requests :)
 const port = process.env.PORT || 80;
 app.listen(port, () => { console.log(`Server is running on.... http://localhost:${port}`) });
